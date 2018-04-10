c43ca82b421115b554dade7744caa86731e4a55f
Sean Francis N. Ballais <sean@seanballais.com>
2018-04-10 13:40:30 +0800

    Completed Google Sign in integration.

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+#class User < ApplicationRecord
+#end
+class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uId = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,12 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2,
+             ENV['UPVTC_CAAS_GOOGLE_CLIENT_ID'],
+             ENV['UPVTC_CAAS_GOOGLE_CLIENT_SECRET'],
+             {
+                 client_options: {
+                    ssl: {
+                        ca_file: Rails.root.join('certs/cacert.pem').to_s
+                    }
+                 }
+             }
+end
